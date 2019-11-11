65a428ae18fcb8610f2a77eb10c17eb036612574
Your Name <you@example.com>
2019-11-11 09:41:33 +0300

    ПрАктуальные правки

--- dev/static/js/product-card.js
+++ dev/static/js/product-card.js
@@ -1,5 +1,37 @@
 $(() => {
 
+
+    /*let $carousel = $('.hardware__slider').slick({
+        variableWidth: true,
+        slidesToShow: 1,
+        dots: false,
+        prevArrow: false,
+        nextArrow: false,
+        infinite: false
+    });*/
+    $('.hardware__slider').owlCarousel({
+        items: 3,
+        loop: false,
+        margin: 15,
+        nav: false,
+        dots: false,
+        scrollbarType: "progress"
+    });
+    const slideCount = $('.hardware__slide').length;
+
+    /*$( ".hardware__range" ).slider({
+        mode: "fast",
+        change: function( event, ui ) {
+            let carousel = $carousel.slick( "getSlick" );
+            let goToSlide = ui.value * (carousel.slideCount-1) / 5;
+            $carousel.slick( "goTo", goToSlide );
+            console.dir(goToSlide);
+        }
+    });*/
+
+
+
+
     // Change door and color
     const colors = $('.catalog-item__color');
     const doors = $('.product-variant__img');
