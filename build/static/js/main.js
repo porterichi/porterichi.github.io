272e60c8593c11d47f925dc7c547c77ff5827e77
Your Name <you@example.com>
2019-11-19 16:55:30 +0300

    правки

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
     svg4everybody({});
+
+    const state = {
+        items: '.catalog-item__color',
+        active: 'catalog-item__color--active'
+    };
+
+    // Catalog UI
+
+    // Change Product Color
+    $('.catalog-item__color').on('click', function(e) {
+        e.preventDefault();
+
+        const el = $(this);
+        const parent = el.parent();
+        const collection = parent.find(state.items);
+
+        if(!el.hasClass(state.active)) {
+
+            collection.removeClass(state.active);
+            el.addClass(state.active);
+        }
+    });
 });
