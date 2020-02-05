94ae8f39b4d2d1fd7abea15e3c7830727a313434
Your Name <you@example.com>
2020-02-05 17:56:09 +0300

    Fix

--- dev/static/js/product-entrance.js
+++ dev/static/js/product-entrance.js
@@ -42,6 +42,7 @@ $(function() {
         $('.create-modal').fadeIn();
         const source = $(this).attr('src');
         $('.create-modal__wrap img').attr('src', source );
+        $('.create-modal__wrap').removeClass('change-modern--modal');
 
     });
 
@@ -51,5 +52,86 @@ $(function() {
     });
 
     //End of create, Gallery
+
+
+
+    // create, Choose
+    $('.entrance-choose__tabs-item').on('click', function() {
+        const self = $(this);
+        const tabs = $('[data-choose-entrance-tab]');
+        const activeTabNumber = self.data('choose-entrance-tab');
+        const blockCollection = $(`[data-choose-entrance-block]`);
+        const activeBlock = $(`[data-choose-entrance-block=${activeTabNumber}]`);
+
+        if( !self.hasClass('active') ) {
+            tabs.removeClass('active');
+            self.addClass('active');
+
+            blockCollection.hide();
+            activeBlock.fadeIn(300);
+        }
+
+
+
+    });
+    //End of Choose
+
+    // pick-accessories Sly Slider
+    var $frame = $('.pick-accessories__slider-wrap');
+    var $wrap = $frame.parent();
+
+    $frame.sly({
+        horizontal: 1,
+        itemNav: 'basic',
+        activateOn: 'click',
+        mouseDragging: 1,
+        touchDragging: 1,
+        startAt: 0,
+        scrollBar: $wrap.find('.scrollbar'),
+        scrollBy: 1,
+        speed: 300,
+        elasticBounds: 1,
+        easing: 'easeOutExpo',
+        dragHandle: 1,
+        dynamicHandle: 1,
+        clickBar: 1,
+    });
+
+    // pick-accessories Sly Slider
+    var controller = new ScrollMagic.Controller();
+
+    // define movement of panels
+    var wipeAnimation = new TimelineMax()
+    // animate to second panel
+        	// move back in 3D space
+        .to("#slideContainer", 1,   {x: -1309})	// move in to first panel
+        			// move back to origin in 3D space
+        // animate to third panel
+
+
+    // create scene to pin and link animation
+    new ScrollMagic.Scene({
+        triggerElement: "#pinContainer",
+        triggerHook: "onLeave",
+        duration: "100%"
+    })
+        .setPin("#pinContainer")
+        .setTween(wipeAnimation)
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+    // define movement of panels
+
+    // change-modern modal
+    $('.change-modern__door').on('click', function () {
+        $('body').addClass('fixed');
+        $('.create-modal').fadeIn();
+        const source = $(this).find('.change-modern__door-img').attr('src');
+        $('.create-modal__wrap').addClass('change-modern--modal');
+        $('.create-modal__wrap img').attr('src', source );
+
+    });
+    // End of change-modern modal
+
 
 });
